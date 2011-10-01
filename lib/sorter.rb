2d7b96f245d847059f9f1a9c101faf1de6f22e64
markvan <markvanharmelen@gmail.com>
2011-10-01 21:16:21 +0100

    now have :add for sorter

--- lib/sorter.rb
+++ lib/sorter.rb
@@ -1,7 +1,7 @@
 class Sorter
   attr_reader :value # either this line or value method
 
-  def initialize(value)
-    @value=3         # simply set instance variable @val to 3
+  def add(value)
+    @value=value         # simply set instance variable @val to 3
   end
 end
--- spec/sorter_spec.rb
+++ spec/sorter_spec.rb
@@ -1,12 +1,14 @@
 require "rspec"
 require '../lib/sorter.rb'
 
-
 describe "Sorter" do
 
   it "should create a new Sorter node with the correct value" do
-    sorter=Sorter.new(3)
+    sorter=Sorter.new
+    sorter.add(3)
     sorter.value.should == 3
+    sorter.add(4)
+    sorter.value.should == 4
   end
 
 end
