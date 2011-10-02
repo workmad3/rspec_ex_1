69481c9daeb81c16ccbc63a08dd3bbf05ad50433
David Workman <workmad3@gmail.com>
2011-10-02 20:07:30 +0100

    A spec suite done, refactored from wrapper around an array to an actual binary tree, but this wasn't required by tests

--- lib/sorted_binary_tree.rb
+++ lib/sorted_binary_tree.rb
@@ -1,21 +1,98 @@
 class SortedBinaryTree
+  def initialize
+    @root = NilNode.new
+  end
+
   def empty?
-    @value.nil?
+    @root.empty?
   end
 
   def add(item)
-    @value = item
+    @root = @root.add(item)
   end
 
-  def contains?(item)
-    @value == item
+  def remove(item)
+    @root = @root.remove(item)
   end
 
-  def get_node(value)
-    self
+  def count
+    @root.count
   end
 
-  def root
-    self
+  def include?(item)
+    @root.include?(item)
+  end
+
+  def to_a
+    @root.to_a
+  end
+
+  class Node
+    def initialize(item)
+      @value = item
+      @left_node = NilNode.new
+      @right_node = NilNode.new
+    end
+
+    def empty?
+      false
+    end
+
+    def remove(item)
+      if item < @value
+        @left_node.remove(item)
+      elsif item > @value
+        @right_node.remove(item)
+      else
+        @value = @right_node.instance_variable_get(:@value)
+      end
+      self
+    end
+
+    def add(item)
+      if item < @value
+        @left_node = @left_node.add(item)
+      else
+        @right_node = @right_node.add(item)
+      end
+      self
+    end
+
+    def count
+      @left_node.count + @right_node.count + 1
+    end
+
+    def include?(item)
+      @value == item || @left_node.include?(item) || @right_node.include?(item)
+    end
+
+    def to_a
+      @left_node.to_a + [@value] + @right_node.to_a
+    end
+  end
+
+  class NilNode
+    def count
+      0
+    end
+
+    def remove(item)
+    end
+
+    def empty?
+      true
+    end
+    
+    def add(item)
+      Node.new(item)
+    end
+
+    def include?(item)
+      false
+    end
+
+    def to_a
+      []
+    end
   end
 end
--- spec/lib/sorted_binary_tree_spec.rb
+++ spec/lib/sorted_binary_tree_spec.rb
@@ -2,35 +2,99 @@ require 'spec_helper'
 
 describe SortedBinaryTree do
   subject {SortedBinaryTree.new}
+  context "adding items" do
+    it "should be empty when no items are added" do
+      subject.should be_empty
+    end
 
-  it "should be empty" do
-    subject.should be_empty
-  end
-  
-  it "should not be empty once an item is added" do
-    subject.add(33)
-    subject.should_not be_empty
+    it "should not be empty when an item is added" do
+      subject.add(33)
+      subject.should_not be_empty
+    end
+
+    it "should have a count of 0 when no items are added" do
+      subject.count.should == 0
+    end
+
+    it "should have a count of 1 when one item is added" do
+      subject.add(33)
+      subject.count.should == 1
+    end
+
+    it "should have a count of 2 when two items are added" do
+      subject.add(33)
+      subject.add(34)
+      subject.count.should == 2
+    end
   end
 
-  it "should contain a number that has been added" do
-    subject.add(33)
-    subject.contains?(33).should be_true
+  context "querying" do
+    it "should return false when no items are added" do
+      subject.include?(33).should be_false
+    end
+
+    it "should return true when 33 is added and 33 is queried for" do
+      subject.add(33)
+      subject.include?(33).should be_true
+    end
+
+    it "should return false when 33 is added and 34 is queried for" do
+      subject.add(33)
+      subject.include?(34).should be_false
+    end
+
+    it "should return true when 33 and 34 are added and 33 is queried for" do
+      subject.add(33)
+      subject.add(34)
+      subject.include?(33).should be_true
+    end
+
+    it "should return true when 33 and 34 are added and 34 is queried for" do
+      subject.add(33)
+      subject.add(34)
+      subject.include?(34).should be_true
+    end
+
+    it "should return false when 33 and 34 are added and 35 is queried for" do
+      subject.add(33)
+      subject.add(34)
+      subject.include?(35).should be_false
+    end
   end
 
-  it "should not contain a number that hasn't been added" do
-    subject.add(33)
-    subject.contains?(34).should_not be_true
+  context "removing items" do
+    before do
+      subject.add(33)
+      subject.add(34)
+      subject.add(35)
+    end
+
+    it "should return false when 33 is removed and 33 is queried for" do
+      subject.remove(33)
+      subject.include?(33).should be_false
+    end
+
+    it "should return true when 33 is removed and 34 is queried for" do
+      subject.remove(33)
+      subject.include?(34).should be_true
+    end
+
+    it "should return true when 33 and 34 are removed and 35 is queried for" do
+      subject.remove(33)
+      subject.remove(34)
+      subject.include?(35).should be_true
+    end
   end
 
-  it "should contain both numbers if two numbers are added" do
-    pending
-    subject.add(33)
-    subject.add(34)
-    (subject.contains?(33) && subject.contains?(34)).should be_true
-  end
+  context "getting sorted output" do
+    before do
+      subject.add(34)
+      subject.add(35)
+      subject.add(32)
+    end
 
-  it "should return the root when getting the only node" do
-    subject.add(33)
-    subject.get_node(33).should == subject.root
+    it "should provide a sorted array with .to_a" do
+      subject.to_a.should == [32, 34, 35]
+    end
   end
 end
